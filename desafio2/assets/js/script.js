c96f3d1284437530827112432e69b26f7d7460fe
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2022-11-07 21:35:23 -0400

    commit 2

---
+++ desafio2/assets/js/script.js
@@ -0,0 +1,31 @@
+//let poke1 = document.querySelector("#poke1");
+//let poke2 = document.querySelector("#poke2");
+//let poke3 = document.querySelector("#poke3");
+
+//let total = poke1 + poke2 + poke3;
+//const verbtn = document.querySelector("#verify"); 
+//const result = document.querySelector("#result")
+//verbtn.addEventListener("click", function() {
+  // result.innerHTML = "Hello";
+  // console.log("exito")
+//});
+
+document.querySelector("#btn").onclick = function() {
+    document.getElementById('demo').innerHTML = "Hello There";
+};
+//function verify() {
+    //document.querySelector("#verify").innerHTML;
+  //  document.querySelector("#verify").addEventListener("click", verify);
+  //}
+//if (total > 10) {
+  //  document.querySelector("#totaltotal").innerHTML = 
+    //          `<div class="card ">
+      //        <h5>Llevas demasiados stickers</h5>
+        //      </div>`;
+//} else
+//{
+  //  document.querySelector("#totaltotal").innerHTML = 
+    //          `<div class="card ">
+      //        <h5>Llevas ${total} stickers</h5>
+        //      </div>`;
+//}
